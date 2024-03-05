bbf8da6e7fe467461b6eb337a6661494622557b7
JunadeMorningstar <junade.dev@gmail.com>
2024-03-05 18:23:20 +0200

    new api folder

---
+++ deepface/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/deepface/healthchecker", methods=["GET"])
+def healthchecker():
+    return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
+
+    @app.route('/deepface/hello')
+    def hello():
+        return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const config = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/deepface/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:5117/api/:path*"
-            : "/api/",
+            ? "http://127.0.0.1:5117/deepface/:path*"
+            : "/deepface/",
       },
     ];
   },
